d1ceaef3a4c023dde1a3e694c5a209b5ed5f3674
Karthik Bhargav <kbhargav@vmware.com>
2016-02-09 18:29:33 +0530

    Added SplashScreenWaitDuration value

--- app/src/main/java/com/ideasunlimited/savingscalculator/Constants.java
+++ app/src/main/java/com/ideasunlimited/savingscalculator/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
 
     public static final String CustomerIdExtraString = "CustomerId";
     public static final String AreaIdExtraString = "AreaId";
+    public static final int SplashScreenWaitTime = 2500;
 }
